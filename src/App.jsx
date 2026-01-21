808c8013a92df7e67103b90b8245721e10db0159
YaciThe <yacintha.theato@googlemail.com>
2026-01-21 10:38:07 +0100

    probe push
    probe

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Wegbegleiter – Probelauf</h1>
+      <h2>ich will das in git pushen</h2>
+      <p>Mein React-Projekt für P3 WS25/26</p>
+    </div>
+  );
+}
+
+export default App;
